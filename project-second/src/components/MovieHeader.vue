64d008635c82dbb61c9b021a1a1a4d2ff1153da5
권찬민 <kwonchanmin@gwonchanmin-ui-MacBookAir.local>
2023-02-03 17:03:24 +0900

    0203 Vue CLI Web 1차Project Result

--- project-second/src/components/MovieHeader.vue
+++ project-second/src/components/MovieHeader.vue
@@ -1,25 +1,26 @@
 <template>
-    <div class="text-bg-dark">
-        <a href="#" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
+     <div class="px-3 py-2 text-bg-dark">
+      <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center ">
+          <a href="#" class="d-flex align-items-center text-white text-decoration-none">
             <svg class="bi me-2" width="40" height="32">
               <use xlink:href="#bootstrap"></use>
             </svg>
             <span class="fs-1" style="color: yellow;" v-on:click="reLoad">Movie BoxOffice Rank</span>
         </a>
-        
+        </div>
+      </div>
     </div>
 </template>
 
 <script>
+
 export default {
     methods:{
-        reLoad() {
-            lacation.reload();
+        reLoad: function () {
+          location.reload();
         }
+
     }
 }
 </script>
-
-<style scoped>
-
-</style>
--- project-second/src/components/MovieSearch.vue
+++ project-second/src/components/MovieSearch.vue
@@ -1,57 +1,174 @@
 <template>
     <div>
-        <input type="date" v-model="searchDate"  placehodler="search" aria-label="search">
-        <button type="button" @click="myFunc">검색</button> 
-        <hr>
+        <div class="px-3 py-2 border-left mb-0 text-bg-dark">
+              <div class="container d-flex flex-wrap jestify-content-center">
+                <form class="col-12 d-flex" role="search">
+                  <input type="date" v-model="searchDate" class=" form-control" placehodler="search" aria-label="search" >
+                  <button type="button" class="col-1 btn btn-outline-light" @click="movieFunc">검색</button> 
+                </form>
+              </div>
+            </div>
+       <!-- Modal -->
+    <div class="modal" id="moviemodal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title text-dark bg-white">{{ detailInfo.movieNm }} </h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <div>
+              <div>
+                <div class="text-dark bg-white"> 제작년도 :{{ detailInfo.prdtYear }}</div>
+              </div>
+              <div v-for="nation in detailInfo.nations" :key="nation.nationNm">
+                <div class="text-dark bg-white"> 제작국가 :{{ nation.nationNm }}</div>
+              </div>
+              <div v-for="director in detailInfo.directors" :key="director.peopleNm">
+                <div class="text-dark bg-white"> 감독 :{{ director.peopleNm }}</div>
+              </div>
+              <div v-for="actor in detailInfo.actors" :key="actor.peopleNm">
+                <div class="text-dark bg-white"> 배우 :{{ actor.peopleNm }}</div>
+              </div>
+              <div class="text-dark bg-white"> 상영시간 :{{ detailInfo.showTm }}</div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
     </div>
+
+
+        <div class="px-3 py-2 text-bg-white text-bg-dark" align="right">
+          <button type="button" class="btn btn-danger" v-on:click="selDelete">선택삭제</button>
+        </div>
+    <div class="table-responsive text-bg-dark">
+      <table class="table table-striped table-sm table table-bordered table table-light table table-hover">
+        <thead>
+          <tr>
+            <th scope="col" class="text-center">선택</th>
+            <th scope="col" class="text-center">순위</th>
+            <th scope="col" class="text-center">포스터</th>
+            <th scope="col" class="text-center">제목</th>
+            <th scope="col" class="text-center">관람객 수</th>
+            <th scope="col" class="text-center">개봉일</th>
+            <th scope="col" class="text-center">삭제</th>
+          </tr>
+        </thead>
+        <tbody class="text-bg-dark">
+          <tr v-for="item in list" :key="item.rank" class="table table-dark">
+            <td><input type="checkbox" :value=item.movieNm v-model="checklist"></td>
+            <td class="text-center">{{ item.rank }}</td>
+            <td><img v-bind:src="imgurl[item.rnum - 1]" /></td>
+            <td class="text-center"> <a href="#" data-bs-toggle="modal" data-bs-target="#moviemodal"
+                v-on:click="searchInfo(item.movieCd)">{{ item.movieNm }}</a> </td>
+            <td class="text-center">{{ item.audiAcc }}</td>
+            <td class="text-center">{{ item.openDt }}</td>
+            <td><button type="button" class="btn btn-danger" v-on:click="removeRow(item.rank)">삭제</button></td>
+          </tr>
+
+        </tbody>
+      </table>
+    </div>
+  </div>
+    
 </template>
 
 <script>
-import MovieTable from '../components/MovieTable.vue'
 
-export default {
-    components:{
-        MovieTable
-    },
+
+export default{
     data() {
         return {
-        list: [],
-        imgurl: [],
-        searchDate: '',
+            list : [],
+            searchDate : '',
+            imgurl:[],
+            checklist : [],
+            detailInfo : []
         }
     },
-    methods: {
-    myFunc: function () {
-          // Axios를 이용한 AJAX 호출
-          let cmp = this;
+    methods:{
+        movieFunc() {
+           let cmp = this;
+            axios({
+                url:'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json',
+                method:'GET',
+                params: {
+                    key:'6e32c1eecd679c968eec7ee87f533a98',
+                    targetDt:this.searchDate.replace(/-/g,'')
+                }
+            })
+            .then(res => {
+                cmp.list= res.data.boxOfficeResult.dailyBoxOfficeList;
+                console.log(cmp.list);
+                let img = this;
+              cmp.list.forEach(function (item, idx) {  
+                axios({
+                  url: 'https://dapi.kakao.com/v2/search/image',
+                  method: 'GET',
+                  params: {
+                    query: item.movieNm + '포스터'
+                  },
+                  headers: {
+                    Authorization: 'KakaoAK 0b5d171371beef2d526a7ae4a14a3eed'
+                  }
+                })
+                  .then(function (result) {
+                    Vue.set(img.imgurl, idx, result.data.documents[2].thumbnail_url); 
+                  })
+                  .catch(function (err) {
+                    console.log(err)
+                  })
+              });
+
+
+            })
+            .catch(function(err){
+                console.log(err);
+                alert('날짜 고를줄 모르냐?')
+            })
+        },
+        removeRow: function (rank) {
+          this.list = this.list.filter((e) => e.rank !== rank);
+          this.checklist = [];
+        },
+        selDelete: function () {
+          this.list = this.list.filter(
+            (item) => !this.checklist.includes(item.movieNm)
+          );
+          this.checklist = [];
+        },
+        searchInfo: function (movieCd) {
+          let esc = this;
           axios({
-            url: 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json',
+            url: 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json',
             method: 'GET',
-            // JQuery에서는 data를 통해 정보를 넘겨줬는데 Vue 에서 data는 POST,PUT,DELETE 메소드 방식에만 적용이 가능
-            // Vue 에서는 params가 URL parameter를 치징한다. Query String으로 전달되는 데이터를 의미하며 당연히 GET방식
-            // JQuery:data = Vue:params
             params: {
               key: '6e32c1eecd679c968eec7ee87f533a98',
-              targetDt: this.searchDate.replace(/-/g, '')
+              movieCd: movieCd
             },
             responseType: 'json' // 기본값
           })
-            .then(res => {  // .then = 성공했을 때 호출
-              cmp.list = res.data.boxOfficeResult.dailyBoxOfficeList;
-              localStorage.setItem(this.list,this.list)
-              this.list=[]
-              console.log(cmp.list)
-                
+            .then(function (res) {
+              esc.detailInfo = res.data.movieInfoResult.movieInfo
             })
-            .catch(function (err) { // .catch() = 실패했을 때 호출
+            .catch(function (err) {
               console.log(err)
-              alert('날짜 고를 줄 모르냐?')
-            });
+            })
         },
-   }
-}
+        reLoad: function () {
+          location.reload();
+        }
+
+      }
+    }
+
 </script>
 
-<style scoped>
-
+<style>
+.right {
+    float: right;
+}
 </style>
